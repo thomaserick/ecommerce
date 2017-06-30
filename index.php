4a95eb8942211f43e16ae019932807445189ec11
TEF <thominhaserick@gmail.com>
2017-06-29 22:12:36 -0300

    Criamos a classe page

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim; 
+use \Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	$page = new Page();
+
+	$page->setTpl("index");
 
 });
 
---
+++ views-cache/header.7de693477577934f8c36ccb729347d3d.rtpl.php
@@ -0,0 +1,6 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><html>
+	<head>
+		<title>Hello</title>
+	</head>
+	<body>
+		
